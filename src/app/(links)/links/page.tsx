f3e700614cbf443376992156399d173511d410fa
nona <multiplestriker@gmail.com>
2025-05-23 17:52:27 +1000

    added simple links page

--- src/app/(links)/links/page.tsx
+++ src/app/(links)/links/page.tsx
@@ -1,5 +1,38 @@
 import styles from "./page.module.css";
+import links from "../../../../content/links.json";
+import Button from "@/components/Button";
+import { socials } from "@/lib/consts";
 
 export default function Home() {
-    return <main className={styles.main}></main>;
+    return (
+        <main className={styles.main}>
+            <h1>
+                {/* TODO LOGO HERE */}
+                KEEBSOC YAYAYAYAYAYAY
+            </h1>
+            <div style={{ width: "100%", maxWidth: 400, display: "flex", flexDirection: "column", gap: 16 }}>
+                {links.map((link: { name: string; link: string }, idx: number) => (
+                    <Button
+                        key={idx}
+                        linkURL={link.link}
+                    >
+                        {link.name}
+                    </Button>
+                ))}
+            </div>
+            <div className={styles.links}>
+                {socials.map((social) => (
+                    <a
+                        className={styles.link}
+                        href={social.href}
+                        key={social.name}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        <div className={styles[`${social.name}`]}></div>
+                    </a>
+                ))}
+            </div>
+        </main>
+    );
 }
