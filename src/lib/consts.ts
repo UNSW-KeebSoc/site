0bfcc5c59db0ea95366482e742b2852b621b4113
hexcyan <101159102+hexcyan@users.noreply.github.com>
2025-05-24 15:12:07 +1000

    add about page

--- src/lib/consts.ts
+++ src/lib/consts.ts
@@ -1,9 +1,4 @@
-export const pages = [
-    "gallery",
-    "events",
-    "blog",
-    "about",
-];
+export const pages = ["gallery", "events", "blog", "about"];
 
 export const socials = [
     {
@@ -24,3 +19,87 @@ export const socials = [
         href: "https://www.facebook.com/unswkeebsoc",
     },
 ];
+
+type ExecutiveRole =
+    | "President"
+    | "Vice President"
+    | "Treasurer"
+    | "Secretary"
+    | "Arc Delegate"
+    | "Returning Officer";
+
+type DirectorRole =
+    | "Projects Director"
+    | "Events Director"
+    | "Marketing Director"
+    | "Creative Director"
+    | "Development Director";
+
+type Execs = {
+    [year: string]: {
+        executives: { position: ExecutiveRole; name: string }[];
+        directors: { position: DirectorRole; name: string }[];
+    };
+};
+
+export const executives: Execs = {
+    "2025": {
+        executives: [
+            { position: "President", name: "firstname lastname" },
+            { position: "Vice President", name: "firstname lastname" },
+            { position: "Treasurer", name: "firstname lastname" },
+            { position: "Secretary", name: "firstname lastname" },
+            { position: "Arc Delegate", name: "firstname lastname" },
+            { position: "Returning Officer", name: "firstname lastname" },
+        ],
+        directors: [
+            { position: "Projects Director", name: "firstname lastname" },
+            { position: "Events Director", name: "firstname lastname" },
+            { position: "Marketing Director", name: "firstname lastname" },
+            { position: "Creative Director", name: "firstname lastname" },
+            {
+                position: "Development Director",
+                name: "firstname lastname",
+            },
+        ],
+    },
+    "2024": {
+        executives: [
+            { position: "President", name: "firstname lastname" },
+            { position: "Vice President", name: "firstname lastname" },
+            { position: "Treasurer", name: "firstname lastname" },
+            { position: "Secretary", name: "firstname lastname" },
+            { position: "Arc Delegate", name: "firstname lastname" },
+        ],
+        directors: [
+            { position: "Marketing Director", name: "firstname lastname" },
+            { position: "Creative Director", name: "firstname lastname" },
+        ],
+    },
+    "2023": {
+        executives: [
+            { position: "President", name: "firstname lastname" },
+            { position: "Vice President", name: "firstname lastname" },
+            { position: "Treasurer", name: "firstname lastname" },
+            { position: "Secretary", name: "firstname lastname" },
+            { position: "Arc Delegate", name: "firstname lastname" },
+        ],
+        directors: [
+            { position: "Marketing Director", name: "firstname lastname" },
+            { position: "Creative Director", name: "firstname lastname" },
+        ],
+    },
+    "2022": {
+        executives: [
+            { position: "President", name: "firstname lastname" },
+            { position: "Vice President", name: "firstname lastname" },
+            { position: "Treasurer", name: "firstname lastname" },
+            { position: "Secretary", name: "firstname lastname" },
+            { position: "Arc Delegate", name: "firstname lastname" },
+        ],
+        directors: [
+            { position: "Marketing Director", name: "firstname lastname" },
+            { position: "Creative Director", name: "firstname lastname" },
+        ],
+    },
+};
